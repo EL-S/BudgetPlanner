5a201cdde10964d2b546d5b0449ccdad3a28a2af
EL-S <minerjohn123@gmail.com>
2019-09-29 17:23:26 +1000

    Created some links to the templates
    a few examples of how to use data passed through the request object is provided, a for loop is also demonstrated to create an html table in the index template

---
+++ Application/views.py
@@ -0,0 +1,15 @@
+# from Application import views
+from django.shortcuts import render_to_response, render
+from django.template import RequestContext
+from django.template.loader import get_template
+
+def index(request):
+    if request.method == 'GET':
+        variables = request.GET
+    elif request.method == 'POST':
+        variables = request.POST
+    return render(request, 'index.html', {'cookies': request.COOKIES, 'files': request.FILES, 'variables': variables, 'request_type': request.method, 'headers': request.headers})
+
+def some_path(request):
+    # this is a comment, if you want a link to this file/function, add it to the urls.py file in the BudgetPlanner directory
+    return render(request, 'template_name.html', {'the_variable_name': 'the variable value'})
