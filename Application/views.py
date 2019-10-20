3df1e5954d5139a8b435ce24550342d4b8e5f77e
EL-S <minerjohn123@gmail.com>
2019-10-21 03:32:01 +1100

    Added some registration code

--- Application/views.py
+++ Application/views.py
@@ -1,7 +1,8 @@
 # from Application import views
-from django.shortcuts import render_to_response, render
+from django.shortcuts import render_to_response, render, redirect
 from django.template import RequestContext
 from django.template.loader import get_template
+import Application.database
 
 def index(request):
     if request.method == 'GET':
@@ -14,7 +15,36 @@ def some_path(request):
     # this is a comment, if you want a link to this file/function, add it to the urls.py file in the BudgetPlanner directory
     return render(request, 'template_name.html', {'the_variable_name': 'the variable value'})
 
-
 def login(request):
     # this is a comment, if you want a link to this file/function, add it to the urls.py file in the BudgetPlanner directory
-    return render(request, 'login.html', {'login': 'usename'})
+    return render(request, 'login.html', {'login': 'username'})
+
+def login_user(request):
+    # perform the login and redirect to the correct page
+    if request.method != 'POST':
+        return redirect('/login')
+    variables = request.POST
+    fields = ['username', 'password']
+    if all(field in variables for field in fields): 
+        print(variables)
+        
+        return redirect('/')
+    else:
+        return render(request, 'login.html', {'error': 'fill in all the fields'})
+
+def register(request):
+    return render(request, 'register.html', {'signup': 'username'})
+
+def register_user(request):
+    # perform the registration and redirect to the correct page
+    if request.method != 'POST':
+        return redirect('/register')
+    variables = request.POST
+    fields = ['username', 'password', 'password_confirm', 'email']
+    if all(field in variables for field in fields): 
+        print(variables)
+        result = Application.database.register_user(variables['username'], variables['email'], variables['password'], variables['password_confirm'])
+        print(result)
+        return redirect('/')
+    else:
+        return render(request, 'register.html', {'error': 'fill in all the fields'})
