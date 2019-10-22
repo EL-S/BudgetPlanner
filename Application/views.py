aa6e8924dfb7ca60e0c8ac410713c9465a75e835
EL-S <minerjohn123@gmail.com>
2019-10-22 23:59:05 +1100

    Show results of database operations

--- Application/views.py
+++ Application/views.py
@@ -15,37 +15,48 @@ def some_path(request):
     # this is a comment, if you want a link to this file/function, add it to the urls.py file in the BudgetPlanner directory
     return render(request, 'template_name.html', {'the_variable_name': 'the variable value'})
 
-def login(request):
+# login page
+def login(request): 
     # this is a comment, if you want a link to this file/function, add it to the urls.py file in the BudgetPlanner directory
     return render(request, 'login.html', {'login': 'username'})
 
+# processes login post request
 def login_user(request):
     # perform the login and redirect to the correct page
     if request.method != 'POST':
         return redirect('/login')
     variables = request.POST
     fields = ['username', 'password']
-    if all(field in variables for field in fields): 
+    if all(field in variables for field in fields): # if the user filled in all the fields
         print(variables)
-        
-        return redirect('/')
+        result = Application.database.login(variables['username'], variables['password'])
+        print(result)
+        if result == "login successful":
+            return redirect('/')
+        else:
+            return render(request, 'login.html', {'error': result})
     else:
         return render(request, 'login.html', {'error': 'fill in all the fields'})
 
+# register page
 def register(request):
     return render(request, 'register.html', {'signup': 'username'})
 
+# processes register post request
 def register_user(request):
     # perform the registration and redirect to the correct page
     if request.method != 'POST':
         return redirect('/register')
     variables = request.POST
     fields = ['username', 'password', 'password_confirm', 'email']
-    if all(field in variables for field in fields): 
+    if all(field in variables for field in fields): # if the user filled in all the fields
         print(variables)
         result = Application.database.register_user(variables['username'], variables['email'], variables['password'], variables['password_confirm'])
         print(result)
-        return redirect('/')
+        if "successful" in result:
+            return render(request, 'index.html', {'result': result})
+        else:
+            return render(request, 'register.html', {'error': result})
     else:
         return render(request, 'register.html', {'error': 'fill in all the fields'})
 
