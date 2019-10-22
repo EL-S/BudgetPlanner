670ec7ef3ad948f0e8bbd6c9c040f04cfb23a3af
adamsurge <102584836@student.swin.edu.au>
2019-10-22 14:47:07 +1100

    Added comments, fixed indenation

--- static/scripts/budget.js
+++ static/scripts/budget.js
@@ -1,5 +1,6 @@
 "use strict"
 
+//Calculates total from table and adds it to the table
 function calcTotal() {
 	var table = document.getElementById('tb');
     var body = table.tBodies[0]
@@ -16,9 +17,10 @@ function calcTotal() {
 	}
 	
 	var htotal = document.getElementById('total');
-	htotal.innerHTML = '$' + total;
+	htotal.innerHTML = '$' + Math.round(total * 100) / 100;
 }
 
+//Deletes a row from the table
 function deleteRow(num) {
 	var table = document.getElementById('tb');
 	var $row = $(document.getElementById('r' + num)).closest("tr"); //Find row
@@ -28,6 +30,27 @@ function deleteRow(num) {
 	calcTotal();
 }
 
+//Gets info from form and adds it to the table
+function addTableRow() {
+	var table = document.getElementById('tb');
+    var body = table.tBodies[0]
+    var rows = body.rows
+		
+	var str = $('form').serialize();
+	var ar = str.replace("bname=", "").replace("&pvalue=", ",").replace("&type=", ",").split(',');
+	
+	//add to database: 
+	var bname = ar[0];
+	var btype = ar[2];
+	var bvalue = ar[1];
+	
+	var i = rows.length - 2;
+	
+	rows[rows.length - 1].insertAdjacentHTML('beforebegin', "<td>" + bname + "</td><td>" + btype + "</td><td>$" + bvalue + "</td><td><form onsubmit='deleteRow(" + i + ")' id='r" + i + "'><button>Delete</button></form></td>");
+	calcTotal();
+}
+
+//Add data from database to table
 function addRowsFromDatabase() {
 	var table = document.getElementById('tb');
     var body = table.tBodies[0]
@@ -39,35 +62,16 @@ function addRowsFromDatabase() {
 	
 	//For each item in database for user
 	//for(var i = 0; i < db.length; i++;) {
-	var td1 = "test"
-	var td2 = "Spending"
-	var td3 = 1
+	var bname = "test"
+	var btype = "Spending"
+	var bvalue = 1
 	
 	var i = 0;
 	
     // pick the last and prepend
-	rows[rows.length - 1].insertAdjacentHTML('beforebegin', "<td>" + td1 + "</td><td>" + td2 + "</td><td>$" + td3 + "</td><td><form onsubmit='deleteRow(" + i + ")' id='r" + i + "'><button>Delete</button></form></td>");
+	rows[rows.length - 1].insertAdjacentHTML('beforebegin', "<td>" + bname + "</td><td>" + btype + "</td><td>$" + bvalue + "</td><td><form onsubmit='deleteRow(" + i + ")' id='r" + i + "'><button>Delete</button></form></td>");
 	// }
 	
-	calcTotal();
-}
-
-function addTableRow() {
-	var table = document.getElementById('tb');
-    var body = table.tBodies[0]
-    var rows = body.rows
-		
-	var str = $('form').serialize();
-	var ar = str.replace("bname=", "").replace("&pvalue=", ",").replace("&type=", ",").split(',');
-	
-	//add to database:
-	var td1 = ar[0];
-	var td2 = ar[2];
-	var td3 = ar[1];
-	
-	var i = rows.length - 2;
-	
-	rows[rows.length - 1].insertAdjacentHTML('beforebegin', "<td>" + td1 + "</td><td>" + td2 + "</td><td>$" + td3 + "</td><td><form onsubmit='deleteRow(" + i + ")' id='r" + i + "'><button>Delete</button></form></td>");
 	calcTotal();
 }
 
